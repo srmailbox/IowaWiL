3229b79755d727709d7fcf77d9b8e418a57dea5b
Serje Robidoux <serje.robidoux@acu.edu.au>
2025-12-04 12:02:41 +1100

    add an assessment of composite models of reading.

--- ReadingCorrelations.R
+++ ReadingCorrelations.R
@@ -28,3 +28,25 @@ iowaReadingCorsbyGrade = tapply(iowaReading, iowaReading$Truegrade
                select(starts_with("CC2"), starts_with("GORT"), -CC2reg)
              , use="pairwise.complete") %>% round(3)
        )
+
+### sem models by grade ####
+
+semMod = '
+  Reading =~ CC2Irr.NW+GORT_Fluency+GORT_Comprehension
+'
+
+# by grade
+iowaG1to6 = iowaReading %>% filter(Truegrade<=6)
+tapply(iowaG1to6, iowaG1to6$Truegrade
+#       , function(x) {cfa(semMod, x, missing="fiml", orthogonal=T, std.lv=T, std.ov=T) %>% parameterestimates() %>% filter(op=="=~")}
+       , function(x) {cfa(semMod2, x, missing="fiml", orthogonal=T, std.lv=T, std.ov=T) %>% lavPredict %>% psych::describe()}
+)
+
+### Split the CC2s
+semMod2 = '
+  Reading =~ CC2irr+CC2nw+GORT_Fluency+GORT_Comprehension
+'
+
+tapply(iowaG1to6, iowaG1to6$Truegrade
+       , function(x) {cfa(semMod2, x, missing="fiml", orthogonal=T, std.lv=T, std.ov=T) %>% lavPredict %>% psych::describe()}
+)
