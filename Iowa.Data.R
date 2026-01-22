51c86a6ec616d95ba535406cb46df79a6f3dbd0b
Serje Robidoux <serje.robidoux@acu.edu.au>
2026-01-22 12:37:23 +1100

    update the data processing to combine reading ability and reading environment data and modify the measurement model to use the data processing script.
    TODO: still missing interest data. To follow up with AS.

--- Iowa.Data.R
+++ Iowa.Data.R
@@ -9,11 +9,14 @@
 
 # 0.0 Setup ####
 include(readxl)
+include(lavaan)
 
-# 1.0 Data ####
-# The data file contains lots of variables that are not relevant here. The 
-# data dictionary includes a column that identifies the ones I need for this 
-# purpose.
+# 1.0 Raw Data ####
+## 1.1 Data Dictionary ###
+
+# The Reading Environment data  contains lots of variables that are not relevant 
+# here. The data dictionary includes a column that identifies the ones I need 
+# for this purpose.
 
 iowaVarDetails = read_xlsx("BobWiL.DataDictionary.xlsx", sheet = "Variables") %>% 
   filter(MeasurementModel) %>% 
@@ -26,7 +29,8 @@ iowaVarDetails = read_xlsx("BobWiL.DataDictionary.xlsx", sheet = "Variables") %>
   )
 msrmntMdlVars = iowaVarDetails$Variable
 
-iowaData = read_xlsx("ReadAnx_Env_item.xlsx", sheet="ReadAnx_Q5_item") %>% 
+## 1.2 Reading Environment Data ####
+iowaEnv = read_xlsx("ReadAnx_Env_item.xlsx", sheet="ReadAnx_Q5_item") %>% 
   select(participant, Sample, StudyYear, StartGrade, StartYear, all_of(msrmntMdlVars)) %>% 
   mutate(StartGrade = as.numeric(substr(StartGrade,1,1))
          , Grade = StartGrade+StudyYear-1)
@@ -34,7 +38,7 @@ iowaData = read_xlsx("ReadAnx_Env_item.xlsx", sheet="ReadAnx_Q5_item") %>%
 # hm... need to check that there were only 3 data points in gr. 7... that seems
 # wrong.
 
-## 1.1 Recoding Schemes ####
+### 1.2.1 Recoding Schemes ####
 # most of the variables were answered on a "likert-like" scale
 # e.g., how often do you X: never, <1 time per month, once a month... etc...
 # I want to recode those into numeric scores, rather than just ordered.
@@ -50,30 +54,30 @@ recodeList = list(
   , TimeRange = c(7, 22, 45, 90, 150)
 )
 
-# 2.0 Recode variables as numeric ####
+### 1.2.2.0 Recode variables as numeric ####
 # This will take each variable, look up the Options for that variable and,
 # if necessary, recode it.
 
-for (var in colnames(iowaData)[6:31]) {
+for (var in colnames(iowaEnv)[6:31]) {
   varOptions = iowaVarDetails$Options[iowaVarDetails$Variable==var]
   if(!is.na(varOptions))
-    iowaData[,var]=recodeList[[varOptions]][unlist(iowaData[,var])]
+    iowaEnv[,var]=recodeList[[varOptions]][unlist(iowaEnv[,var])]
   
 }
 
-# str(iowaData)
+# str(iowaEnv)
 
-## 2.1 Duration fields ####
+### 1.2.3 Duration fields ####
 # Unfortunately, the duration fields were input as strings which means they have
 # a lot of non-numeric values that will need to be converted to numeric.
 
 # this bit of code spits out a list of the responses provided across the three
 # duration_ fields:
-# iowaData %>% select(starts_with("duration")) %>% unlist %>% as.matrix %>% 
+# iowaEnv %>% select(starts_with("duration")) %>% unlist %>% as.matrix %>% 
 #   unique %>% 
 #   write.csv(file="durationResponses.csv", row.names = F, quote=T)
 
-# iowaData %>% 
+# iowaEnv %>% 
 #   select(homework, reading_school_related
 #          , reading_nonschool_related, internet_reading) %>% 
 #   unlist %>% as.matrix %>% unique %>% 
@@ -88,10 +92,8 @@ durationRespMap = read_xlsx("durationResponseMap.xlsx", sheet="duration_fields")
          , duration_read_alone_mins = duration) %>% 
   select(-duration, -`Hm?`)
 
-
-
-for (var in colnames(iowaData)[which(substr(colnames(iowaData), 1, 4)=="dura")]) {
-  iowaData = merge(iowaData
+for (var in colnames(iowaEnv)[which(substr(colnames(iowaEnv), 1, 4)=="dura")]) {
+  iowaEnv = merge(iowaEnv
                    , durationRespMap %>% 
                      select(strResp, all_of(paste0(var, "_mins")))
                    , by.x=var, by.y="strResp", all.x=T
@@ -99,3 +101,58 @@ for (var in colnames(iowaData)[which(substr(colnames(iowaData), 1, 4)=="dura")])
   
 }
 
+### 1.2.4 Derive Reading Interest and Frequency Scores ####
+
+#### 1.2.4.1 Reading Interest ####
+# We pre-registered just using the mean of the 5 CMQ items:
+#  - I do not appear to have this data available to me.
+
+### 1.2.4.2 Frequency (Shared Book Reading & Independent Reading) ####
+# We had decided to use the results from a latent variable model with 
+# Independent Reading and child-active SBR as the main measures of frequency.
+# (that is, only consider reading where the child is actively reading, not
+# passively being read to)
+# 
+# See Iowa.SBRMeasurementModel.R for details of this decision
+
+cfaFreqModel = '
+# F1 - child reading to parent: child_read_*
+childSBR =~ child_read_out_loud+child_read_picture_book+child_read_comics+child_read_chapter_books+child_read_nonfiction
+
+# F2 - independent reading: read_alone, ..._chapter_book, initiate_read_alone (nonschool_related, duration_read_alone)
+IR =~ read_alone+read_alone_chapter_books+read_alone_comics+initiate_read_alone+reading_nonschool_related+duration_read_alone_mins
+
+# F3 - parent reading to child: parent_read_out_loud, .._chapter_books (picture_books, nonfiction)
+# prntSBR =~ parent_read_out_loud+parent_read_chapter_books#+parent_read_picture_books+parent_read_nonfiction#+parent_read_comics
+
+# F4 - school work: homework, reading school related - IGNORED since homework includes more than just reading
+
+childSBR~~IR #+ prntSBR
+# IR ~~ prntSBR
+'
+cfaData = iowaEnv %>% 
+  drop_na(child_read_out_loud,child_read_picture_book,child_read_comics,
+          child_read_chapter_books,child_read_nonfiction,read_alone
+          ,read_alone_chapter_books,read_alone_comics,initiate_read_alone,
+          reading_nonschool_related,duration_read_alone_mins)
+
+iowa.cfaFreq = sem(cfaFreqModel, data=cfaData, std.ov = T)
+
+#fitmeasures(iowa.cfaFreq, fit.measures = c("srmr", "cfi", "agfi", "rmsea"))
+# mediocre fit, really
+
+# Assign scores to kids and years
+cfaRes = cbind(
+  cfaData %>% 
+    select(participant, StudyYear)
+  , predict(iowa.cfaFreq)) %>% 
+  rename(Fi = IR, Fsbr = childSBR)
+
+iowaEnv=merge(iowaEnv, cfaRes, all.x=T)
+
+## 1.3 Reading Ability ####
+iowaReading = read_xlsx("ReadAnx_Scores.xlsx", sheet="ReadAnx_Scores") %>% 
+  select(participantID, Sample, Truegrade, starts_with("CC2"), starts_with("GORT"))
+
+iowaData = merge(iowaEnv, iowaReading, by.x=c("participant", "Grade")
+                 , by.y=c("participantID", "Truegrade"), all=T)
--- Iowa.SBRMeasurementModel.R
+++ Iowa.SBRMeasurementModel.R
@@ -15,6 +15,7 @@
 # Created: 2025-10-24
 # Author: Serje Robidoux
 # CHANGELOG:
+# 2026-01-22 Modified to use a script for data processing.
 # 
 
 # 0.0 Setup ####
@@ -22,93 +23,10 @@ include(lavaan)
 include(readxl)
 
 # 1.0 Data ####
-# The data file contains lots of variables that are not relevant here. The 
-# data dictionary includes a column that identifies the ones I need for this 
-# purpose.
-
-iowaVarDetails = read_xlsx("BobWiL.DataDictionary.xlsx", sheet = "Variables") %>% 
-  filter(MeasurementModel) %>% 
-  mutate(
-    Options = str_split_i(Options, pattern="\\|", i = 1)
-    , Options = trimws(str_split_i(Options, pattern=",", i=2))
-    , Options = ifelse(Options=="Never", "Monthly", Options)
-    , Options = ifelse(Options=="0-25%", "Proportion", Options)
-    , Options = ifelse(Options=="less than 15 minutes", "TimeRange", Options)
-  )
-msrmntMdlVars = iowaVarDetails$Variable
-
-iowaData = read_xlsx("ReadAnx_Env_item.xlsx", sheet="ReadAnx_Q5_item") %>% 
-  select(participant, Sample, StudyYear, StartGrade, StartYear, all_of(msrmntMdlVars)) %>% 
-  mutate(StartGrade = as.numeric(substr(StartGrade,1,1))
-         , Grade = StartGrade+StudyYear-1)
-
-# hm... need to check that there were only 3 data points in gr. 7... that seems
-# wrong.
-
-## 1.1 Recoding Schemes ####
-# most of the variables were answered on a "likert-like" scale
-# e.g., how often do you X: never, <1 time per month, once a month... etc...
-# I want to recode those into numeric scores, rather than just ordered.
-
-recodeList = list(
-  # If the first option is "Never", then the frequency is expressed in momthly/
-  # weekly/daily terms, so I convert them assuming a 4wk month (28 days)
-  Monthly = c(0, .5, 1, 3, 4, 10, 28, 35)
-  # if the first option is 0-25%, then the data is expressed as a proportion
-  # of times.
-  , Proportion = c(.125, .375, .625, .875)
-  # These are expressed in minutes (less than 15, 15-30, 30-60, 1-2hr, 2hr+)
-  , TimeRange = c(7, 22, 45, 90, 150)
-  )
-
-# 2.0 Recode variables as numeric ####
-# This will take each variable, look up the Options for that variable and,
-# if necessary, recode it.
-
-for (var in colnames(iowaData)[6:31]) {
-  varOptions = iowaVarDetails$Options[iowaVarDetails$Variable==var]
-  if(!is.na(varOptions))
-    iowaData[,var]=recodeList[[varOptions]][unlist(iowaData[,var])]
-    
-}
-
-str(iowaData)
-
-## 2.1 Duration fields ####
-# Unfortunately, the duration fields were input as strings which means they have
-# a lot of non-numeric values that will need to be converted to numeric.
-
-# this bit of code spits out a list of the responses provided across the three
-# duration_ fields:
-# iowaData %>% select(starts_with("duration")) %>% unlist %>% as.matrix %>% 
-#   unique %>% 
-#   write.csv(file="durationResponses.csv", row.names = F, quote=T)
-
-# iowaData %>% 
-#   select(homework, reading_school_related
-#          , reading_nonschool_related, internet_reading) %>% 
-#   unlist %>% as.matrix %>% unique %>% 
-#   write.csv(file="afterSchool_amountoftime.csv", row.names=F, quote=T)
-
-
-#### NOTE: there is one value of 200 here that has been replaced with 20, after
-#### # examining the child's other responses
-durationRespMap = read_xlsx("durationResponseMap.xlsx", sheet="duration_fields") %>% 
-  mutate(duration_shared_reading_mins = duration
-         , duration_read_out_loud_mins = duration
-         , duration_read_alone_mins = duration) %>% 
-  select(-duration, -`Hm?`)
-
-
-
-for (var in colnames(iowaData)[which(substr(colnames(iowaData), 1, 4)=="dura")]) {
-  iowaData = merge(iowaData
-                   , durationRespMap %>% 
-                     select(strResp, all_of(paste0(var, "_mins")))
-                   , by.x=var, by.y="strResp", all.x=T
-                   )
-
-}
+# 2.0 Duration fields ####
+# These two sections have been moved and encapsulated in a separate scripts to
+# be shared by all analyses.
+source("Iowa.Data.R")
 
 # 3.0 Measurement Models ####
 # OK now that we have useful data, we can start building measurement models to
@@ -199,7 +117,7 @@ mmF4 = '
 
 ### 3.2.1 single factor ####
 
-iowaF1 = cfa(mmF1, data=iowaData, std.ov=T)
+iowaF1 = cfa(mmF1, data=iowaEnv, std.ov=T)
 fitmeasures(iowaF1, fit.measures = c("srmr", "cfi", "agfi", "rmsea"))
 # terrible fit
 #  srmr   cfi  agfi rmsea 
@@ -208,7 +126,7 @@ fitmeasures(iowaF1, fit.measures = c("srmr", "cfi", "agfi", "rmsea"))
 
 ### 3.2.2 two factor ####
 
-iowaF2 = cfa(mmF2, data=iowaData, std.ov=T)
+iowaF2 = cfa(mmF2, data=iowaEnv, std.ov=T)
 fitmeasures(iowaF2, fit.measures = c("srmr", "cfi", "agfi", "rmsea"))
 
 # No real improvement
@@ -218,7 +136,7 @@ fitmeasures(iowaF2, fit.measures = c("srmr", "cfi", "agfi", "rmsea"))
 
 ### 3.2.3 Four factor ####
 
-iowaF4 = cfa(mmF4, data=iowaData, std.ov=T)
+iowaF4 = cfa(mmF4, data=iowaEnv, std.ov=T)
 fitmeasures(iowaF4, fit.measures = c("srmr", "cfi", "agfi", "rmsea"))
 
 # More noticeable improvement, but still not a "good" fit.
@@ -233,14 +151,14 @@ anova(iowaF1, iowaF2, iowaF4)
 
 ### 3.2.5 Exploratory/Factor Analysis ####
 psych::scree(
-  iowaData %>% 
+  iowaEnv %>% 
     select(-participant, -Sample, -StudyYear, -StartGrade, -StartYear
            , -Grade, -starts_with("duration"), ends_with("_mins")) %>% 
     drop_na())
 
 # hm. Scree plot says 3 factors, PCA says as many as 8 components.
 # looking at the factors, 4 seems to make some sense.
-factanal(iowaData %>% 
+factanal(iowaEnv %>% 
            select(-participant, -Sample, -StudyYear, -StartGrade, -StartYear
                   , -Grade, -starts_with("duration"), ends_with("_mins")) %>% 
            drop_na()
@@ -256,7 +174,7 @@ factanal(iowaData %>%
 # F4 - school work: homework, reading school related
 
 ### 3.2.6 PCA ####
-(iowa.pca = prcomp(iowaData %>% 
+(iowa.pca = prcomp(iowaEnv %>% 
          select(-participant, -Sample, -StudyYear, -StartGrade, -StartYear
                 , -Grade, -starts_with("duration"), ends_with("_mins")) %>% 
          drop_na()))$rotation %>% round(2)
@@ -296,11 +214,11 @@ IR =~ read_alone+read_alone_chapter_books+read_alone_comics+initiate_read_alone+
 IR ~~ SBR
 '
 
-iowa.cfaRev = sem(cfaRevModel, data=iowaData, std.ov = T)
+iowa.cfaRev = sem(cfaRevModel, data=iowaEnv, std.ov = T)
 fitmeasures(iowa.cfaRev, fit.measures = c("srmr", "cfi", "agfi", "rmsea"))
 
 
-iowa.cfaRevAlt = sem(cfaRevModelAlt, data=iowaData, std.ov = T)
+iowa.cfaRevAlt = sem(cfaRevModelAlt, data=iowaEnv, std.ov = T)
 fitmeasures(iowa.cfaRevAlt, fit.measures = c("srmr", "cfi", "agfi", "rmsea"))
 
 include(semPlot)
@@ -308,3 +226,5 @@ semPaths(iowa.cfaRevAlt, whatLabels = "est")
 semPaths(iowa.cfaRev, whatLabels = "est")
 
 parameterEstimates(iowa.cfaRevAlt) %>% filter(rhs=="SBR")
+
+predict(iowa.cfaRev)
