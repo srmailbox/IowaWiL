f03afbda0cab15b69d5ba83a6f6ed004c7495633
serjerobidoux <serje.robidoux@gmail.com>
2026-03-09 11:48:20 +1100

    adjust variable names to allow comparison between SEM derived means, and calculated means.

--- Year3.CMQ.DataPatterns.R
+++ Year3.CMQ.DataPatterns.R
@@ -6,7 +6,7 @@ merge(iowaInterest %>%
   group_by(StudyYear) %>% summarize(n=n())
   , by="StudyYear") %>% 
   mutate(patternP = round(nPattern/n,3)) %>% 
-  arrange(pick(starts_with("motiv")))
+  arrange(pick(starts_with("motiv"))) #%>% arrange(StudyYear)
 
 # There's something odd about Year 3, where the response rate overall is huge
 # (98.4%) but very few provide a full set of data (53.7% - compared to 75-100%
@@ -16,3 +16,8 @@ merge(iowaInterest %>%
 ### 2026-03-07: New file provided, fixes issues with missingness in Year 5, but
 ### the Year 3 issue remains - it's been verified that these are the data, but
 ### it's not clear why it occurred. Change in RedCap system perhaps?
+### 
+
+q2.gr46.SEMAnalysis %>% 
+  select(starts_with("motiv")) %>% 
+  summarise(across(starts_with("motiv"), ~mean(is.na(.)))) %>% t()
